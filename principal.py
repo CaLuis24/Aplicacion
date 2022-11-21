4f250634b4c473280ade0f8e8e0c5271cd2e2e74
CaLuis24 <zcarlosluis06@gmail.com>
2022-11-21 11:42:58 -0600

    commit

---
+++ principal.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.title("Hola Fede")
+st.header("Qu'est-ce que? Pysho killer!")
+st.write("Hola mundo")
+st.markdown("[Good music](https://www.youtube.com/watch?v=CKti7QixnJI)")
